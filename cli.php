13d8552a016bf5100d103d7689853c0184f42651
Vincent Daligault <ligro@daligro.net>
2014-12-19 19:44:57 +0100

    cli read action from cli

--- cli.php
+++ cli.php
@@ -5,17 +5,31 @@ require_once __DIR__ . '/action.php';
 
 $usage = "{$argv[0]} [-h] [--help] action\n";
 
-if (count($argv) == 1 || isset($argv['-h']) || isset($argv['--help'])) {
+if (isset($argv['-h']) || isset($argv['--help'])) {
     echo $usage;
     exit(1);
 }
 
-if (count($argv) != 2) {
+if (count($argv) > 2) {
     echo "bad number of arguments.\n$usage";
     exit (1);
 }
 
-$action = array_pop($argv);
+if (count($argv) == 1) {
+    do {
+        $action = readline("action > ");
+        if ($action == '?') {
+            echo implode("\n", Action::$whitelist);
+        }
+    } while ($action == '?');
+} else {
+    $action = array_pop($argv);
+}
+
+if ($action == 'quit') {
+    exit(0);
+}
+
 $data = readline("data > ");
 
 try {
