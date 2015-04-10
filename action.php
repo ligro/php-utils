b73e9bc0e9210580ce73dce45476a47a4941be0e
Vincent Daligault <ligro@daligro.net>
2015-04-10 11:35:15 +0200

    better error message for json_decode

--- action.php
+++ action.php
@@ -36,19 +36,27 @@ class Action
     public static function json_decode_array($data)
     {
         $value = json_decode($data, true);
-        if ($error = json_last_error()) {
-            throw new Exception($error);
-        }
-
+        self::_jsonError();
         return $value;
     }
     public static function json_decode($data)
     {
         $value = json_decode($data);
-        if ($error = json_last_error()) {
-            throw new Exception($error);
-        }
-
+        self::_jsonError();
         return $value;
+    }
+
+    protected static function _jsonError()
+    {
+        if ($code = json_last_error()) {
+            switch ($code) {
+            case 4:
+                $msg = "Syntax error";
+                break;
+            default:
+                $msg = $code;
+            }
+            throw new Exception($msg, $code);
+        }
     }
 }
